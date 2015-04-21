d2d9fc18601f3a2cd737ed1627df2cb28fe79571
oscar90y5 <oscarfernandezangulo@gmail.com>
2015-04-21 10:47:28 +0200

    corregido color del toolBar

--- CargasPuntuales/app/src/main/java/com/cargaspuntuales/ui/adapters/ContactAdapter.java
+++ CargasPuntuales/app/src/main/java/com/cargaspuntuales/ui/adapters/ContactAdapter.java
@@ -54,6 +54,7 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
         public ContactViewHolder(View v) {
             super(v);
             vName = (TextView) v.findViewById(R.id.txtName);
+
             vSurname = (TextView) v.findViewById(R.id.txtSurname);
             vEmail = (TextView) v.findViewById(R.id.txtEmail);
             vTitle = (TextView) v.findViewById(R.id.title);
